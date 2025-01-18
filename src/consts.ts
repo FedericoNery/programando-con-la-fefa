dae6005d26b2854e2931f3fd02f3b935dd8a657b
Federico Nery <fedegnery@gmail.com>
2025-01-18 20:02:39 -0300

    Changed content and added python collection

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Programando con La Fefa';
+export const SITE_DESCRIPTION = 'Bienvenido al blog de programación de La Fefa';
--- src/content.config.ts
+++ src/content.config.ts
@@ -12,7 +12,25 @@ const blog = defineCollection({
 		pubDate: z.coerce.date(),
 		updatedDate: z.coerce.date().optional(),
 		heroImage: z.string().optional(),
+		heroWidth: z.number().optional(),
+		heroHeight: z.number().optional(),
 	}),
 });
 
-export const collections = { blog };
+const python = defineCollection({
+	// Load Markdown and MDX files in the `src/content/blog/` directory.
+	loader: glob({ base: './src/content/python', pattern: '**/*.{md,mdx}' }),
+	// Type-check frontmatter using a schema
+	schema: z.object({
+		title: z.string(),
+		description: z.string(),
+		// Transform string to Date object
+		pubDate: z.coerce.date(),
+		updatedDate: z.coerce.date().optional(),
+		heroImage: z.string().optional(),
+		heroWidth: z.number().optional(),
+		heroHeight: z.number().optional(),
+	}),
+});
+
+export const collections = { blog, python };
